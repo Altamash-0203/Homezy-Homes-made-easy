0a767d51988912b036c0509d5b03447cb277d273
Altamash Shaikh <mr.shaikh.altamash0203@gmail.com>
2025-11-10 19:37:44 +0530

    DB topology

--- Backend/index.js
+++ Backend/index.js
@@ -9,9 +9,7 @@ const propertyRoutes = require("./Routes/PropertyRoutes");
 dotenv.config();
 const app = express();
 
-app.use(cors({
-    origin:
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
