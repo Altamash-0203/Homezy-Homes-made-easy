73d37ce74e05eb175fb27a7f99c54330e93edbc9
Altamash Shaikh <mr.shaikh.altamash0203@gmail.com>
2025-11-10 20:04:02 +0530

    env update

--- Backend/index.js
+++ Backend/index.js
@@ -18,6 +18,7 @@ app.use("/api/users", authRoutes);
 app.use("/api/properties", propertyRoutes);
 
 // Connect to MongoDB
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
